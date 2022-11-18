b378be2d39e9730084a089b1a055337d4ee66520
ChaseIRL <112039422+vk-chase@users.noreply.github.com>
2022-11-17 20:45:49 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,21 +3,10 @@ game 'gta5'
 
 
 client_scripts {
-	'fishing_cl.lua',
-	'hunting_cl.lua',
-	'blipies.lua',
-	'menustuff.lua',
-	'treasurebox_cl.lua',
-	'metalbox_cl.lua',
-	'usablefishingloots_cl.lua',
-	'targets.lua'
+	'client/**.lua',
 }
 
 
 server_scripts {
-	'fishing_sv.lua',
-	'treasurebox_sv.lua',
-	'metalbox_sv.lua',
-	'usablefishingloots_sv.lua',
-	'hunting_sv.lua'
+	'server/**.lua',
 }
