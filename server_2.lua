9b04be4178a74da21939c1362a3c5f521d0bf138
ChaseIRL <112039422+vk-chase@users.noreply.github.com>
2022-12-26 15:50:49 -0600

    update fish sell feature

--- server_2.lua
+++ server_2.lua
@@ -694,5 +694,3 @@ RegisterNetEvent('chasefishingloot:server:afterfishingloot', function()
         TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['metaltrash'], "add")
     end
 end)
-
-
