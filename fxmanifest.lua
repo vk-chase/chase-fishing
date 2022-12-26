1c8a93b089ceb3ba7babf66dbd132c95d789f950
ChaseIRL <112039422+vk-chase@users.noreply.github.com>
2022-12-26 15:55:43 -0600

    fixed ,

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,6 @@ game 'gta5'
 
 --feel free to share etc i dont care enjoy it but keep it free please
 
-client_scripts {'client_main.lua','client_2.lua','client_3.lua','sell_mainclient.lua'	'@PolyZone/client.lua','@PolyZone/BoxZone.lua','@PolyZone/ComboZone.lua'}
+client_scripts {'client_main.lua','client_2.lua','client_3.lua','sell_mainclient.lua','@PolyZone/client.lua','@PolyZone/BoxZone.lua','@PolyZone/ComboZone.lua'}
 server_scripts {'server_main.lua','server_2.lua','@oxmysql/lib/MySQL.lua','sell_mainserver.lua'}
 shared_scripts {'@qb-core/shared/locale.lua','config.lua','locales/en.lua'}
