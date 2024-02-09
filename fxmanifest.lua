d31e8d2e0085b9b2afd962c800631ca27648e699
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2024-02-09 14:30:13 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,7 @@
-
 fx_version "cerulean"
 game "gta5"
 lua54 'yes'
 server_scripts {'@oxmysql/lib/MySQL.lua','server/*.lua'}
 shared_scripts {'@qb-core/shared/locale.lua','locales/en.lua','config.lua'}
 client_scripts {'client/*.lua'}
-dependency 'qb-core'
-dependency 'qb-inventory'
-dependency 'qb-menu'
-dependency 'qb-target'
-dependency 'ps-ui'
-escrow_ignore {'config.lua', 'readme/**.txt','readme/**.png', 'client/c_outdoors_openclient.lua', 'locales/*.lua'}
 
