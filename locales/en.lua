45731f60ca7a9d119050e374e13d5f605c75e9c1
VK-Chase <112039422+vk-chase@users.noreply.github.com>
2024-09-25 12:36:02 -0500

    Update en.lua
    v2

--- locales/en.lua
+++ locales/en.lua
@@ -7,44 +7,34 @@ local Translations = {
         negative = 'Trying to sell a negative amount?',
         no_items = 'Not enough items',
         no_license = 'You do not have %{value} license',
-
-        --lost fish 
-        okokcaughtfishtitle = 'Fishing', -- the title of the notificaiton if you use okok (now also used for RTX header Notifications)
         lostfish = 'Fish got away!..',
-
         --lures missing
-        missingchub = 'You dont have a Chub Shiner!...',
-        missingfrog = 'You dont have a Plastic Frog!...',
-        missingspook = 'You dont have a Super Spook TopWater!...',
-        missingtwitch = 'You dont have a InShore Twitch Lure!...',
-        missingeel = 'You dont have a Blue Eel!...',
-        missingsquid = 'You dont have a Squid Lure!...',
-        missingstinger = 'You dont have any Stinger Bait!...',
-        missingswolfy = 'You dont have a Swolfy Big Fish!...',
-        missingskipper = 'You dont have a Yellow Skipper!...',
-        missingsnakehead = 'You dont have a Snakehead Peche Iscas!...',
-        missingdryfly = 'You dont have a Dry Fly Lure!...',
-
+        missingone = 'You are missing Fly Fly Bait!...',
+        missingtwo = 'You dont have a Chub Shiner!...',
+        missingthree = 'You dont have a Plastic Frog!...',
+        missingfour = 'You dont have a Super Spook TopWater!...',
+        missingfive = 'You dont have a InShore Twitch Lure!...',
+        missingsix = 'You dont have a Blue Eel!...',
+        missingseven = 'You dont have a Squid Lure!...',
+        missingeight = 'You dont have any Stinger Bait!...',
+        missingnine = 'You dont have a Swolfy Big Fish!...',
+        missingten = 'You dont have a Yellow Skipper!...',
+        missingeleven = 'You dont have a Snakehead Peche Iscas!...',
         notwading = 'Make sure you are wading, try a creek, river or even swampy area!...',
         checkyoself = 'Make sure you face water and are on a sturdy surface!...',
-
         --errors for broke bitches
         notenoughmoneybank = 'You dont have enough money in your bank for this!..',
         notenoughmoneycash = 'You dont have enough cash on you for this!..',
-
         polebreak = 'Your Fishing Rod just snapped!...',
-
     },
     success = {
         --sell shop
         sold = 'You have sold %{value} x %{value2} for $%{value3}',
-
         --anchor
         gettinganchor = 'Retrieving Anchor!...',
         stowanchor = 'Anchor Disabled and Stowed!...',
         dropanchor = 'Dropping Anchor!...',
         lockedanchor = 'Anchor is locked in!...',
-
         tackleboxopen = 'Grabbing Bait..',
         savedbait = 'Managed to save the bait off that cast!...',
         rentalheader = 'Outdoor Rentals',-- only for okok / rtx notifications
@@ -66,7 +56,6 @@ local Translations = {
         skipper ='Yellow Skipper',
         snakehead ='Snakehead Peche Iscas',
         closemenu ='< Close',
-        
     },
     info = { 
         -- sell shop
@@ -74,6 +63,8 @@ local Translations = {
         open_return = 'Open the Goods Shop',
         sell = 'Sell Goods',
         sell_return = 'Sell Goods to Shop',
+        no_fish = 'No Fish to Sell',
+        no_fish_desc = 'You don\'t have any fish in your inventory to sell.',
         fish_closed = 'Store is closed. Come back between %{value}:00 AM - %{value2}:00 PM',
         sell_items = 'Selling Price $%{value}',
         back = '⬅ Go Back',
@@ -82,24 +73,15 @@ local Translations = {
         okoknotsoldfishtitle = 'Could Not Sell',
         lostabigone = 'You Just Lost a big catch!!!',
         -- catching fish success
-        okokcaughtfishtitle = 'Fishing', -- the title of the notificaiton if you use okok 
         caughtfish = 'Fish Caught..',
-        
         --misc
         boatrentheader = 'Boat Rental',
         boatreturnheader = 'Return Boat Rental',
         atvrentalheader = 'ATV Rental',
         atvreturnheader = 'Return ATV Rental',
-
         atvreturntomenu = 'Return to Main Menu',
         boatreturnheader = 'Return Boat Rental',
-
-
-
-
     },
-
-
 }
 
 Lang = Locale:new({
