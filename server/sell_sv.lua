11953a78a1cd4f42078d8c7166a3839cd74725a8
VK-Chase <112039422+vk-chase@users.noreply.github.com>
2024-09-25 12:31:19 -0500

    Update sell_sv.lua

--- server/sell_sv.lua
+++ server/sell_sv.lua
@@ -1,57 +1,54 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local function SendDiscordWebhook(player, itemName, itemAmount, totalPrice)
+    local embed = {
+        {
+            ["title"] = "Fish Sold",
+            ["color"] = 65280,   
+            ["fields"] = {
+                {["name"] = "Player Name", ["value"] = player.PlayerData.charinfo.firstname .. " " .. player.PlayerData.charinfo.lastname, ["inline"] = true},
+                {["name"] = "Citizen ID", ["value"] = player.PlayerData.citizenid, ["inline"] = true},
+                {["name"] = "Item", ["value"] = QBCore.Shared.Items[itemName].label, ["inline"] = true},
+                {["name"] = "Amount", ["value"] = itemAmount, ["inline"] = true},
+                {["name"] = "Total Price", ["value"] = "$" .. totalPrice, ["inline"] = true}
+            },
+            ["footer"] = {
+                ["text"] = "Fish Selling System"
+            },
+            ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ")
+        }
+    }
+
+    PerformHttpRequest(Config.discordWebhookURL, function(err, text, headers) end, 'POST', json.encode({embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
 
 RegisterNetEvent('c_Fish:server:SellReturnies', function(itemName, itemAmount, itemPrice)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local totalPrice = (tonumber(itemAmount) * itemPrice)
+
     if tonumber(itemAmount) == 1 and Player.Functions.RemoveItem(itemName, tonumber(itemAmount)) then
         if Config.BankMoney then
             Player.Functions.AddMoney('bank', totalPrice)
-            TriggerClientEvent('c_Fish:client:openMenu',src)
         else
             Player.Functions.AddMoney('cash', totalPrice)
-            TriggerClientEvent('c_Fish:client:openMenu',src)
         end
-        if Config.NotifyType == 'qb' then
-            TriggerClientEvent('QBCore:Notify', src, Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 'success', 3500)
-        end
-        if Config.NotifyType == 'okok' then
-            TriggerClientEvent('okokNotify:Alert', source, Lang:t('info.okoksoldfishtitle'), Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 3500, 'success')
-        end
-        if Config.NotifyType == 'rtx' then
-           TriggerClientEvent("rtx_notify:Notify", source, Lang:t('info.okoksoldfishtitle'), Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 5000, "success")            
-        end
+        TriggerClientEvent('c_Fish:client:openMenu', src)
+        TriggerClientEvent('QBCore:Notify', src, Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 'success', 3500)
         TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[itemName], 'remove')
+        SendDiscordWebhook(Player, itemName, itemAmount, totalPrice)
     elseif Player.Functions.RemoveItem(itemName, tonumber(itemAmount)) then
         if Config.BankMoney then
             Player.Functions.AddMoney('bank', totalPrice)
-            Wait(200)
-            TriggerClientEvent('c_Fish:client:openMenu',src)
         else
             Player.Functions.AddMoney('cash', totalPrice)
-            Wait(200)
-            TriggerClientEvent('c_Fish:client:openMenu',src)
         end
-        if Config.NotifyType == 'qb' then
-            TriggerClientEvent('QBCore:Notify', src, Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 'success', 3500)
-        end
-        if Config.NotifyType == 'okok' then
-            TriggerClientEvent('okokNotify:Alert', source, Lang:t('info.okoksoldfishtitle'), Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 3500, 'success')
-        end
-        if Config.NotifyType == 'rtx' then
-           TriggerClientEvent("rtx_notify:Notify", source, Lang:t('info.okoksoldfishtitle'), Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 5000, "success")            
-        end
+        Wait(200)
+        TriggerClientEvent('c_Fish:client:openMenu', src)
+        TriggerClientEvent('QBCore:Notify', src, Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 'success', 3500)
         TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[itemName], 'remove')
+        SendDiscordWebhook(Player, itemName, itemAmount, totalPrice)
     else
-        if Config.NotifyType == 'qb' then
-            TriggerClientEvent('QBCore:Notify', src, Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 'success', 3500)
-        end
-        if Config.NotifyType == 'okok' then
-            TriggerClientEvent('okokNotify:Alert', source, Lang:t('info.okoknotsoldfishtitle'), Lang:t('success.sold', { value = tonumber(itemAmount), value2 = QBCore.Shared.Items[itemName].label, value3 = totalPrice }), 3500, 'success')
-        end
-        if Config.NotifyType == 'rtx' then
-           TriggerClientEvent("rtx_notify:Notify", source, Lang:t('info.okoknotsoldfishtitle'), Lang:t('error.no_items'), 5000, "error")            
-        end
+        TriggerClientEvent('QBCore:Notify', src, Lang:t('error.no_items'), 'error', 3500)
     end
     TriggerClientEvent('c_Fish:client:openMenu', src)
 end)
